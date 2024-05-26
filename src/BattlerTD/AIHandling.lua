265f96d395b5e58a82edc5a5b3f7567c418b4f77
Gavin <gavin.monkeystudios@gmail.com>
2024-05-26 11:45:24 -0600

    Refining AI

--- src/BattlerTD/AIHandling.lua
+++ src/BattlerTD/AIHandling.lua
@@ -9,72 +9,59 @@ function ProcessAIForPlayersEvent()
 end
 
 function ProcessAIOrderForPlayer(playerIndex)
-    -- print("Processing AI for player" .. playerIndex)
-
     if humanBuildPriorities[playerAIOrders[playerIndex]] == nil then
         return
     end
 
     local orderType = humanBuildPriorities[playerAIOrders[playerIndex]].type
-    -- print("Order type: " .. orderType)
 
     if orderType == "worker" then
-        -- print("Processing worker order type")
-
         if GetPlayerState(Player(playerIndex - 1), PLAYER_STATE_RESOURCE_GOLD) >= GetUnitGoldCost(FourCC("nvlk")) then
-            IssueImmediateOrderById(playerBaseBuildings[playerIndex], FourCC("nvlk"))
-            -- playerAIOrders[playerIndex] = playerAIOrders[playerIndex] + 1
+            if GetPlayerState(Player(playerIndex - 1), PLAYER_STATE_RESOURCE_FOOD_USED) < GetPlayerState(Player(playerIndex - 1), PLAYER_STATE_RESOURCE_FOOD_CAP) then
+                IssueImmediateOrderById(playerBaseBuildings[playerIndex], FourCC("nvlk"))
+            else
+                playerAIOrders[playerIndex] = playerAIOrders[playerIndex] + 1
+            end
         end
         return
     end
 
     if orderType == "building" then
-        -- print("Processing building order type")
-
         if GetPlayerState(Player(playerIndex - 1), PLAYER_STATE_RESOURCE_GOLD) >= GetUnitGoldCost(FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].building)) and
            GetPlayerState(Player(playerIndex - 1), PLAYER_STATE_RESOURCE_LUMBER) >= GetUnitWoodCost(FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].building)) then
 
             local commanderGroup = GetUnitsOfPlayerMatching(Player(playerIndex - 1), Condition(function()
                 local com = GetUnitTypeId(GetFilterUnit())
-                return com == FourCC("u000:uaco") or com == FourCC("uaco")
+                return com == FourCC("u000:uaco") or com == FourCC("uaco") or (IsUnitType(GetFilterUnit(), UNIT_TYPE_STRUCTURE) and com ~= FourCC("etrp"))
             end))
 
             ForGroup(commanderGroup, function()
-                -- print("Issuing build order for commander")
-
-                IssueBuildOrderById(
+                if IssueBuildOrderById(
                     GetEnumUnit(),
                     FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].building),
                     GetRandomInt(GetRectMinX(playerBuildAreas[playerIndex]), GetRectMaxX(playerBuildAreas[playerIndex])),
                     GetRandomInt(GetRectMinY(playerBuildAreas[playerIndex]), GetRectMaxY(playerBuildAreas[playerIndex]))
-                )
+                ) == false then
+                    IssueImmediateOrderById(GetEnumUnit(), FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].building))
+                end
             end)
-            
-            -- playerAIOrders[playerIndex] = playerAIOrders[playerIndex] + 1
         end
         return
     end
 
     if orderType == "upgrade" then
-        print("Processing upgrade order type")
         local currentLevel = GetPlayerTechCountSimple(FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].upgrade), Player(playerIndex - 1))
         local upgradeFrom = humanBuildPriorities[playerAIOrders[playerIndex]].level
 
-        print("Current count p" .. playerIndex .. ": " .. currentLevel)
-        print("Upgrade from: " .. upgradeFrom)
         if currentLevel == upgradeFrom then
-            print("Order upgrade for player: " .. playerIndex)
             IssueImmediateOrderById(playerBaseBuildings[playerIndex], FourCC(humanBuildPriorities[playerAIOrders[playerIndex]].upgrade))
         else
-            print("Increment order for player: " .. playerIndex)
             playerAIOrders[playerIndex] = playerAIOrders[playerIndex] + 1
         end
-            -- playerAIOrders[playerIndex] = playerAIOrders[playerIndex] + 1
         return
     end
 end
 
 function IncrementAIOrderEvent(player)
     playerAIOrders[GetPlayerId(player) + 1] = playerAIOrders[GetPlayerId(player) + 1] + 1
-    print("Incremended order for " .. GetPlayerId(player) + 1 .. " new order " .. playerAIOrders[GetPlayerId(player) + 1])
 end GetOwningPlayer(GetTriggerUnit())
---
+++ src/BattlerTD/HumanBuildOrder.lua
@@ -0,0 +1,241 @@
+function InitializeHumanAI()
+    humanBuildPriorities = {
+        {["type"] = "worker"}, -- Child
+        {["type"] = "worker"},
+        {
+            ["type"] = "building",
+            ["building"] = "h003:hhou" -- Footman
+        },
+        {["type"] = "worker"},
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 0
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h003:hhou" -- Footman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {
+            ["type"] = "building",
+            ["building"] = "h005:hhou" -- Priest
+        },
+        {["type"] = "worker"},
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 1
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h003:hhou" -- Footman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h003:hhou" -- Footman
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 2
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 3
+        },
+        {["type"] = "worker"},
+        {
+            ["type"] = "building",
+            ["building"] = "h005:hhou" -- Priest
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 4
+        },
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 5
+        },
+        {["type"] = "worker"},
+        {["type"] = "worker"},
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Resw", -- Base Attack
+            ["level"] = 0
+        },
+        {["type"] = "worker"},
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Resw", -- Base Attack
+            ["level"] = 1
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 6
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Resw", -- Base Attack
+            ["level"] = 2
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rerh", -- Base Health
+            ["level"] = 0
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 7
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rhan", -- Child Speed
+            ["level"] = 8
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rerh", -- Base Health
+            ["level"] = 1
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h004:hhou" -- Rifleman
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "upgrade",
+            ["upgrade"] = "Rerh", -- Base Health
+            ["level"] = 2
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h008:hhou" -- Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+        {
+            ["type"] = "building",
+            ["building"] = "h009:hhou" -- Storm Hammers Gryphon Rider
+        },
+    }
+
+    playerAIOrders = {}
+
+    for player = 1, 6 do
+        playerAIOrders[player] = 1
+    end
+end
